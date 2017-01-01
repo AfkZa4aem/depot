fdba1cc145176d43f0f5801d8ee99c4df4a4d582
afkza4aem <nastypinagrably@gmail.com>
2017-01-01 16:22:28 +0300

    add simple validate
    blank - not allowed
    price >= 0.01
    image format - GIF, JPG or PNG
    title - uniq

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ActiveRecord::Base
+  # check title, description, image and price - not empty
+  validates :title, :description, :image_url, presence: true
+  # set price >= 0.01
+  validates :price, numericality: {greater_then_or_equal_to: 0.01}
+  # check title uniq
+  validates :title, uniqueness: true
+  # check image format (gif, jpg, png allowed)
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'URL should link to GIF, JPG or PNG image format.'
+  }
 end
