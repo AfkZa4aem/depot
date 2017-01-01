038f14ba769b5b7035ed70eb78bf8145994c754d
afkza4aem <nastypinagrably@gmail.com>
2017-01-01 13:56:33 +0300

    Db:migrate
    change price type + migrate

--- db/migrate/20170101104756_create_products.rb
+++ db/migrate/20170101104756_create_products.rb
@@ -4,7 +4,8 @@ class CreateProducts < ActiveRecord::Migration
       t.string :title
       t.text :description
       t.string :image_url
-      t.decimal :price
+      # change price type: 01234567.89
+      t.decimal :price, precision: 8, scale: 2
 
       t.timestamps null: false
     end
